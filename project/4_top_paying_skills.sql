66536e157061d61c13ba929e27634fcda62eea2f
Vincent Fernandez <vincent.fndz@gmail.com>
2024-03-27 11:32:40 +0100

    Update comments

--- project/4_top_paying_skills.sql
+++ project/4_top_paying_skills.sql
@@ -1,5 +1,5 @@
 /*
-Answer: What are the top skills based on salary?
+Question: What are the top skills based on salary?
 - Look at the average salary associated with each skill for Data Analyst positions
 - Focuses on roles with specified salaries, regardless of location
 - Why? It reveals how different skills impact salary levels for Data Analysts and 
@@ -24,9 +24,17 @@ LIMIT 25;
 
 /*
 Here's a breakdown of the results for top paying skills for Data Analysts:
-- High Demand for Big Data & ML Skills: Top salaries are commanded by analysts skilled in big data technologies (PySpark, Couchbase), machine learning tools (DataRobot, Jupyter), and Python libraries (Pandas, NumPy), reflecting the industry's high valuation of data processing and predictive modeling capabilities.
-- Software Development & Deployment Proficiency: Knowledge in development and deployment tools (GitLab, Kubernetes, Airflow) indicates a lucrative crossover between data analysis and engineering, with a premium on skills that facilitate automation and efficient data pipeline management.
-- Cloud Computing Expertise: Familiarity with cloud and data engineering tools (Elasticsearch, Databricks, GCP) underscores the growing importance of cloud-based analytics environments, suggesting that cloud proficiency significantly boosts earning potential in data analytics.
+
+- High Demand for Big Data & ML Skills: Top salaries are commanded by analysts skilled in big data technologies (PySpark, Couchbase),
+machine learning tools (DataRobot, Jupyter), and Python libraries (Pandas, NumPy),
+reflecting the industry's high valuation of data processing and predictive modeling capabilities.
+
+- Software Development & Deployment Proficiency: Knowledge in development and deployment tools (GitLab, Kubernetes, Airflow) 
+  indicates a lucrative crossover between data analysis and engineering, with a premium on skills that facilitate automation and
+  efficient data pipeline management.
+
+- Cloud Computing Expertise: Familiarity with cloud and data engineering tools (Elasticsearch, Databricks, GCP) underscores the
+ growing importance of cloud-based analytics environments, suggesting that cloud proficiency significantly boosts earning potential in data analytics.
 
 [
   {
