4825a4e49cc40d7eb8e8d94162d9a30d6ad2c147
Vincent Fernandez <vincent.fndz@gmail.com>
2024-04-05 15:15:38 +0200

    Fix SQL queries

--- project/4_top_paying_skills.sql
+++ project/4_top_paying_skills.sql
@@ -1,9 +1,5 @@
 /*
 Question: What are the top skills based on salary?
-- Look at the average salary associated with each skill for Data Analyst positions
-- Focuses on roles with specified salaries, regardless of location
-- Why? It reveals how different skills impact salary levels for Data Analysts and 
-    helps identify the most financially rewarding skills to acquire or improve
 */
 
 SELECT 
@@ -14,8 +10,8 @@ INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
-    AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = True 
+    AND salary_year_avg IS NOT NULL 
+    AND job_location = 'Anywhere'
 GROUP BY
     skills
 ORDER BY
--- project/5_optimal_skills.sql
+++ project/5_optimal_skills.sql
@@ -1,57 +1,7 @@
 /*
 Question: What are the most optimal skills to learn ?
-- Identify skills in high demand and associated with high average salaries for Data Analyst roles
-- Concentrates on remote positions with specified salaries
-- Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
-    offering strategic insights for career development in data analysis
 */
 
--- Identifies skills in high demand for Data Analyst roles (Query #3)
-WITH skills_demand AS (
-    SELECT
-        skills_dim.skill_id,
-        skills_dim.skills,
-        COUNT(skills_job_dim.job_id) AS demand_count
-    FROM job_postings_fact
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE
-        job_title_short = 'Data Analyst' 
-        AND salary_year_avg IS NOT NULL
-    GROUP BY
-        skills_dim.skill_id
-), 
--- Skills with high average salaries for Data Analyst roles (Query #4)
-average_salary AS (
-    SELECT 
-        skills_job_dim.skill_id,
-        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
-    FROM job_postings_fact
-    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    WHERE
-        job_title_short = 'Data Analyst'
-        AND salary_year_avg IS NOT NULL
-    GROUP BY
-        skills_job_dim.skill_id
-)
--- Return high demand and high salaries for 10 skills 
-SELECT
-    skills_demand.skill_id,
-    skills_demand.skills,
-    demand_count,
-    avg_salary
-FROM
-    skills_demand
-INNER JOIN  average_salary ON skills_demand.skill_id = average_salary.skill_id
-WHERE  
-    demand_count > 10
-ORDER BY
-    avg_salary DESC,
-    demand_count DESC
-LIMIT 30;
-
--- rewriting this same query more concisely
 SELECT 
     skills_dim.skill_id,
     skills_dim.skills,
@@ -63,6 +13,7 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
+    AND job_location = 'Anywhere'
 GROUP BY
     skills_dim.skill_id
 HAVING
@@ -75,194 +26,191 @@ LIMIT 30;
 /*
 Here's a breakdown of the most optimal skills for Data Analysts in 2023: 
 
-Among the top skills in demand, Apache Kafka stands out with a demand count of 40 and an average salary of $129,999. 
-PyTorch follows with a demand count of 20 and an average salary of $125,226, while Perl and TensorFlow share a similar demand count of 20,
-with average salaries of $124,686 and $120,647, respectively. Notably, skills like Apache Airflow and Scala, despite lower demand 
-counts (71 and 59, respectively), offer competitive average salaries of $116,387 and $115,480, indicating their significance in specialized roles.
-Furthermore, cloud technologies such as Google Cloud Platform (GCP) and Snowflake demonstrate high demand counts (78 and 241, respectively),
-correlating with slightly lower average salaries of $113,065 and $111,578. This suggests a robust demand for cloud-related skills but with
-slightly less premium compensation compared to niche technologies like Kafka or PyTorch.
+- **High-Demand Programming Languages:** Python and R.
+- **Cloud Tools and Technologies:** Snowflake, Azure, AWS, and BigQuery.
+- **Business Intelligence and Visualization Tools:** Tableau and Looker.
+- **Database Technologies:** SQL and NoSQL databases (Oracle, SQL Server, NoSQL).
 
 [
   {
-    "skill_id": 98,
-    "skills": "kafka",
-    "demand_count": "40",
-    "avg_salary": "129999"
-  },
-  {
-    "skill_id": 101,
-    "skills": "pytorch",
-    "demand_count": "20",
-    "avg_salary": "125226"
-  },
-  {
-    "skill_id": 31,
-    "skills": "perl",
-    "demand_count": "20",
-    "avg_salary": "124686"
-  },
-  {
-    "skill_id": 99,
-    "skills": "tensorflow",
-    "demand_count": "24",
-    "avg_salary": "120647"
-  },
-  {
-    "skill_id": 63,
-    "skills": "cassandra",
-    "demand_count": "11",
-    "avg_salary": "118407"
-  },
-  {
-    "skill_id": 219,
-    "skills": "atlassian",
-    "demand_count": "15",
-    "avg_salary": "117966"
-  },
-  {
-    "skill_id": 96,
-    "skills": "airflow",
-    "demand_count": "71",
-    "avg_salary": "116387"
-  },
-  {
-    "skill_id": 3,
-    "skills": "scala",
-    "demand_count": "59",
-    "avg_salary": "115480"
-  },
-  {
-    "skill_id": 169,
-    "skills": "linux",
-    "demand_count": "58",
-    "avg_salary": "114883"
+    "skill_id": 8,
+    "skills": "go",
+    "demand_count": "27",
+    "avg_salary": "115320"
   },
   {
     "skill_id": 234,
     "skills": "confluence",
-    "demand_count": "62",
-    "avg_salary": "114153"
-  },
-  {
-    "skill_id": 95,
-    "skills": "pyspark",
-    "demand_count": "49",
-    "avg_salary": "114058"
-  },
-  {
-    "skill_id": 62,
-    "skills": "mongodb",
-    "demand_count": "26",
-    "avg_salary": "113608"
-  },
-  {
-    "skill_id": 18,
-    "skills": "mongodb",
-    "demand_count": "26",
-    "avg_salary": "113608"
-  },
-  {
-    "skill_id": 81,
-    "skills": "gcp",
-    "demand_count": "78",
-    "avg_salary": "113065"
-  },
-  {
-    "skill_id": 92,
-    "skills": "spark",
-    "demand_count": "187",
-    "avg_salary": "113002"
-  },
-  {
-    "skill_id": 193,
-    "skills": "splunk",
-    "demand_count": "15",
-    "avg_salary": "112928"
-  },
-  {
-    "skill_id": 75,
-    "skills": "databricks",
-    "demand_count": "102",
-    "avg_salary": "112881"
-  },
-  {
-    "skill_id": 210,
-    "skills": "git",
-    "demand_count": "74",
-    "avg_salary": "112250"
-  },
-  {
-    "skill_id": 80,
-    "skills": "snowflake",
-    "demand_count": "241",
-    "avg_salary": "111578"
-  },
-  {
-    "skill_id": 6,
-    "skills": "shell",
-    "demand_count": "44",
-    "avg_salary": "111496"
-  },
-  {
-    "skill_id": 168,
-    "skills": "unix",
-    "demand_count": "37",
-    "avg_salary": "111123"
+    "demand_count": "11",
+    "avg_salary": "114210"
   },
   {
     "skill_id": 97,
     "skills": "hadoop",
-    "demand_count": "140",
-    "avg_salary": "110888"
+    "demand_count": "22",
+    "avg_salary": "113193"
   },
   {
-    "skill_id": 93,
-    "skills": "pandas",
-    "demand_count": "90",
-    "avg_salary": "110767"
+    "skill_id": 80,
+    "skills": "snowflake",
+    "demand_count": "37",
+    "avg_salary": "112948"
   },
   {
-    "skill_id": 137,
-    "skills": "phoenix",
-    "demand_count": "23",
-    "avg_salary": "109259"
+    "skill_id": 74,
+    "skills": "azure",
+    "demand_count": "34",
+    "avg_salary": "111225"
   },
   {
-    "skill_id": 25,
-    "skills": "php",
-    "demand_count": "29",
-    "avg_salary": "109052"
+    "skill_id": 77,
+    "skills": "bigquery",
+    "demand_count": "13",
+    "avg_salary": "109654"
   },
   {
-    "skill_id": 58,
-    "skills": "neo4j",
+    "skill_id": 76,
+    "skills": "aws",
+    "demand_count": "32",
+    "avg_salary": "108317"
+  },
+  {
+    "skill_id": 4,
+    "skills": "java",
+    "demand_count": "17",
+    "avg_salary": "106906"
+  },
+  {
+    "skill_id": 194,
+    "skills": "ssis",
     "demand_count": "12",
-    "avg_salary": "108646"
-  },
-  {
-    "skill_id": 2,
-    "skills": "nosql",
-    "demand_count": "108",
-    "avg_salary": "108331"
-  },
-  {
-    "skill_id": 141,
-    "skills": "express",
-    "demand_count": "96",
-    "avg_salary": "108221"
-  },
-  {
-    "skill_id": 78,
-    "skills": "redshift",
-    "demand_count": "90",
-    "avg_salary": "107969"
+    "avg_salary": "106683"
   },
   {
     "skill_id": 233,
     "skills": "jira",
-    "demand_count": "145",
-    "avg_salary": "107931"
+    "demand_count": "20",
+    "avg_salary": "104918"
+  },
+  {
+    "skill_id": 79,
+    "skills": "oracle",
+    "demand_count": "37",
+    "avg_salary": "104534"
+  },
+  {
+    "skill_id": 185,
+    "skills": "looker",
+    "demand_count": "49",
+    "avg_salary": "103795"
+  },
+  {
+    "skill_id": 2,
+    "skills": "nosql",
+    "demand_count": "13",
+    "avg_salary": "101414"
+  },
+  {
+    "skill_id": 1,
+    "skills": "python",
+    "demand_count": "236",
+    "avg_salary": "101397"
+  },
+  {
+    "skill_id": 5,
+    "skills": "r",
+    "demand_count": "148",
+    "avg_salary": "100499"
+  },
+  {
+    "skill_id": 78,
+    "skills": "redshift",
+    "demand_count": "16",
+    "avg_salary": "99936"
+  },
+  {
+    "skill_id": 187,
+    "skills": "qlik",
+    "demand_count": "13",
+    "avg_salary": "99631"
+  },
+  {
+    "skill_id": 182,
+    "skills": "tableau",
+    "demand_count": "230",
+    "avg_salary": "99288"
+  },
+  {
+    "skill_id": 197,
+    "skills": "ssrs",
+    "demand_count": "14",
+    "avg_salary": "99171"
+  },
+  {
+    "skill_id": 92,
+    "skills": "spark",
+    "demand_count": "13",
+    "avg_salary": "99077"
+  },
+  {
+    "skill_id": 13,
+    "skills": "c++",
+    "demand_count": "11",
+    "avg_salary": "98958"
+  },
+  {
+    "skill_id": 186,
+    "skills": "sas",
+    "demand_count": "63",
+    "avg_salary": "98902"
+  },
+  {
+    "skill_id": 7,
+    "skills": "sas",
+    "demand_count": "63",
+    "avg_salary": "98902"
+  },
+  {
+    "skill_id": 61,
+    "skills": "sql server",
+    "demand_count": "35",
+    "avg_salary": "97786"
+  },
+  {
+    "skill_id": 9,
+    "skills": "javascript",
+    "demand_count": "20",
+    "avg_salary": "97587"
+  },
+  {
+    "skill_id": 183,
+    "skills": "power bi",
+    "demand_count": "110",
+    "avg_salary": "97431"
+  },
+  {
+    "skill_id": 0,
+    "skills": "sql",
+    "demand_count": "398",
+    "avg_salary": "97237"
+  },
+  {
+    "skill_id": 215,
+    "skills": "flow",
+    "demand_count": "28",
+    "avg_salary": "97200"
+  },
+  {
+    "skill_id": 201,
+    "skills": "alteryx",
+    "demand_count": "17",
+    "avg_salary": "94145"
+  },
+  {
+    "skill_id": 199,
+    "skills": "spss",
+    "demand_count": "24",
+    "avg_salary": "92170"
   }
 ]
 */
